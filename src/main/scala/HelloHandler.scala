e625fe3d56a59a9da12c6efdce56178298116f7e
Boris Capitanu <borice@hotmail.com>
2016-03-23 12:36:55 -0500

    TSTGF-5: added welcome message for Marie

--- src/main/scala/HelloHandler.scala
+++ src/main/scala/HelloHandler.scala
@@ -55,9 +55,7 @@ class HelloHandler extends Actor {
       sb.append(s"Scala: $scalaVersion").append("\n")
       sb.append(s"SHA $gitSha on branch $gitBranch with version $gitVersion").append("\n")
       sb.append(s"Built on: $builtAtString").append("\n")
-      sb.append(s"\nA friendly message\n")
-      sb.append(s"...and another message\n")
-      sb.append(s"...and a final message\n")
+      sb.append(s"\nThe HTRC is happy to welcome Marie to the project! Welcome, Marie!!! :-)\n")
 
       event.response.write(sb.toString)
       context.stop(self)
