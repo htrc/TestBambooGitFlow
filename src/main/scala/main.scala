45ef316b9d82bda629c208bd50629fb5a4f8575a
Boris Capitanu <borice@hotmail.com>
2016-03-23 12:34:36 -0500

    - added copyright messages

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,17 +1,52 @@
-import org.mashupbots.socko.routes._
-import org.mashupbots.socko.infrastructure.Logger
-import org.mashupbots.socko.webserver.WebServer
-import org.mashupbots.socko.webserver.WebServerConfig
+/*
+ * University of Illinois/NCSA
+ * Open Source License
+ *
+ * Copyright (c) 2016 University of Illinois at Urbana-Champaign.
+ * All rights reserved.
+ *
+ * Developed by:
+ *
+ *     HTRC Dev Team
+ *     HathiTrust Research Center
+ *     https://www.hathitrust.org/htrc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal with
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimers.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimers in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *     * Neither the names of the LLVM Team, University of Illinois at
+ *       Urbana-Champaign, nor the names of its contributors may be used to
+ *       endorse or promote products derived from this Software without specific
+ *       prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE
+ * SOFTWARE.
+ */
 
-import akka.actor.ActorSystem
-import akka.actor.Props
-import akka.actor.ExtensionId
-import akka.actor.ExtensionIdProvider
-import akka.actor.ExtendedActorSystem
+import akka.actor._
+import org.mashupbots.socko.routes._
+import org.mashupbots.socko.webserver.{WebServer, WebServerConfig}
 
 object MyWebServerConfig extends ExtensionId[WebServerConfig] with ExtensionIdProvider {
-  override def lookup = MyWebServerConfig
-  override def createExtension(system: ExtendedActorSystem) =
+  override def lookup: ExtensionId[_ <: Extension] = MyWebServerConfig
+
+  override def createExtension(system: ExtendedActorSystem): WebServerConfig =
     new WebServerConfig(system.settings.config, "config")
 }
 
@@ -29,6 +64,8 @@ object Main extends App {
   webServer.start()
 
   Runtime.getRuntime.addShutdownHook(new Thread {
-    override def run { webServer.stop() }
+    override def run(): Unit = {
+      webServer.stop()
+    }
   })
 }
